53a3bc1f70e57cf13ececff70b9e9c44264ab79d
Dušan Reljić <reljicd@gmail.com>
2017-05-21 10:43:59 +0200

    Initial Commit

---
+++ src/main/java/com/reljicd/SecuredLoginRegistrationTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.reljicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredLoginRegistrationTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredLoginRegistrationTemplateApplication.class, args);
+	}
+}
